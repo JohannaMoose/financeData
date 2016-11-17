b8c07e834e14ba93061d798a8200d0e829622764
Johanna Petersson <min.mail@johannap.se>
2016-11-17 16:23:54 +0100

    Settings file added and read.

--- FinanceData/FinanceData/Program.cs
+++ FinanceData/FinanceData/Program.cs
@@ -15,10 +15,30 @@ namespace FinanceData
 
         static void Main(string[] args)
         {
-            var file = args[0];
+            var settingsFile = new Uri(new Uri(AppDomain.CurrentDomain.BaseDirectory), "settings.txt");
+            if (!File.Exists(settingsFile.AbsolutePath))
+            {
+                Console.WriteLine("Välkommen, det verkar som det är första gången du kör programmet, vi börjar med några inställningar.");
+                Console.WriteLine("Vad är filvägen till excelarket att jobba med. Börja med C: eller motsvarande hela vägen till filen: ");
+                var filePath = Console.ReadLine();
+                while (!File.Exists(filePath))
+                {
+                    Console.WriteLine("Filen kunde inte hittas, vänligen skapa filen om den inte finns och ange rätt väg igen: ");
+                    filePath = Console.ReadLine();
+                }
+
+                Console.WriteLine("Filen hittades, läggs in i inställningar som filen att använda");
+                var settingsFileWriter = new StreamWriter(settingsFile.AbsolutePath);
+                settingsFileWriter.WriteLine(filePath);
+                settingsFileWriter.Flush();
+                settingsFileWriter.Close();
+            }
+
+            var settingsFileReader = new StreamReader(settingsFile.AbsolutePath);
+            var excelFilePath = new Uri(settingsFileReader.ReadLine());
         
             Console.WriteLine("Välkommen, till hämtning av finans data. Komihåg att excelfilen måste vara stängd och inte används av någon annan process för att programmet ska fungera");
-            var excelFilePath = new Uri(file);
+           
             Console.WriteLine("Läser in data från filen {0}", excelFilePath.AbsolutePath);
             var excelFile = new ExcelPackage(new FileInfo(excelFilePath.AbsolutePath));
             var stockList = createStockList(excelFile);
@@ -30,16 +50,29 @@ namespace FinanceData
                 Console.WriteLine("Hämtar data för aktien {0}", stockName);
                 var request = new RestRequest(
               $"/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(\"{stockName}\")%0A%09%09&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback=", Method.GET);
-                var rawData = client.Execute(request).Content;
-                var json = JObject.Parse(rawData);
+                var respnse = client.Execute(request);
+                if (respnse.Content == "")
+                {
+                    Console.WriteLine(
+                        "Det uppstod problem med att kontakta servern och programmet fungerar inte just nu.");
+                }
+                else
+                {
+                    var json = JObject.Parse(respnse.Content);
 
-                var q = json["query"]["results"]["quote"];
+                    var q = json["query"]["results"]["quote"];
 
-                var stock = JsonConvert.DeserializeObject<Stock>(q.ToString());
-                dataSheet.InsertRow(2,1);
-                dataSheet.Cells[2, 1].Value = stock.Symbol;
-                dataSheet.Cells[2, 2].Value = stock.LastTradePriceOnly;
-                dataSheet.Cells[2, 3].Value = DateTime.Now;
+                    var stock = JsonConvert.DeserializeObject<Stock>(q.ToString());
+                    dataSheet.InsertRow(2, 1);
+                    dataSheet.Cells[2, 1].Value = stock.Symbol;
+                    dataSheet.Cells[2, 2].Value = stock.TradeDate;
+                    dataSheet.Cells[2, 3].Value = stock.EarningsShare;
+                    dataSheet.Cells[2, 4].Value = stock.DaysLow;
+                    dataSheet.Cells[2, 5].Value = stock.DaysHigh;
+                    dataSheet.Cells[2, 6].Value = stock.LastTradePriceOnly;
+                    dataSheet.Cells[2, 7].Value = stock.Open;
+                    dataSheet.Cells[2, 8].Value = DateTime.Now;
+                }
             }
 
             Console.WriteLine("Sparar all data som är tillagd i filen");
